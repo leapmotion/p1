c1f0c685c1c04b92aeb32686297950eaf2973da4
Wilbur Yu <yuwilbur@gmail.com>
2014-10-06 14:39:34 -0700

    Added scene changing

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    // Implement Fading for easier transition
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+      Application.LoadLevel("Stage1");
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+      Application.LoadLevel("Stage2");
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha3))
+    {
+      Application.LoadLevel("Stage3"); 
+    }
+	}
+}
