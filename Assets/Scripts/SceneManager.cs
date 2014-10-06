0634cba9477d0aa51d96bc6c4deb4e9e2b6457a2
Wilbur Yu <yuwilbur@gmail.com>
2014-10-06 15:10:49 -0700

    Added fading during scene transitions

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class SceneManager : MonoBehaviour {
 
+  Texture2D texture_;
+  private float timer_ = 0.0f;
+  private float timer_threshold_ = 120.0f;
+  private float timer_increment_ = 1.0f;
+  private string stage_to_load_ = "Stage1";
+
 	// Use this for initialization
 	void Start () {
-	
+    Rect pixelInset = new Rect(0, 0, Screen.width, Screen.height);
+    guiTexture.color = Color.black;
+    guiTexture.pixelInset = pixelInset;
 	}
 	
 	// Update is called once per frame
@@ -13,15 +21,40 @@ public class SceneManager : MonoBehaviour {
     // Implement Fading for easier transition
     if (Input.GetKeyDown(KeyCode.Alpha1))
     {
-      Application.LoadLevel("Stage1");
+      stage_to_load_ = "Stage1";
+      timer_increment_ = -1.0f;
     }
     else if (Input.GetKeyDown(KeyCode.Alpha2))
     {
-      Application.LoadLevel("Stage2");
+      stage_to_load_ = "Stage2";
+      timer_increment_ = -1.0f;
     }
     else if (Input.GetKeyDown(KeyCode.Alpha3))
     {
-      Application.LoadLevel("Stage3"); 
+      stage_to_load_ = "Stage3";
+      timer_increment_ = -1.0f;
     }
+
+    if (timer_ < 0.0f)
+    {
+      Application.LoadLevel(stage_to_load_);
+    }
+
+    timer_ += timer_increment_;
+    if (timer_ > timer_threshold_)
+    {
+      timer_ = timer_threshold_;
+    }
+    Debug.Log(timer_);
 	}
+
+  
+
+  void OnGUI()
+  {
+    float alpha = 1.0f - Mathf.Clamp(timer_ / timer_threshold_, 0.0f, 1.0f);
+    Color color = guiTexture.color;
+    color.a = alpha;
+    guiTexture.color = color;
+  }
 }
