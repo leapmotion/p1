e676ca557971895d8e3bda7e48e39d746c9b571d
David Edelhart <dave@wonderlandlabs.com>
2014-10-07 12:05:05 -0700

    adding a label property stub to ten key

--- Assets/TenKeyKey.cs
+++ Assets/TenKeyKey.cs
@@ -5,7 +5,64 @@ namespace P1
 {
 		public class TenKeyKey : MonoBehaviour
 		{
-		public TextMesh label;
+				string label_ = "";
+
+				public string label {
+						get {
+								return label_;
+						}
+
+						set {
+								switch (value) {
+								case "1": 
+										break;
+					
+					
+								case "2": 
+										break;
+					
+					
+								case "3": 
+										break;
+					
+					
+								case "4": 
+										break;
+					
+					
+								case "5": 
+										break;
+					
+					
+								case "6": 
+										break;
+					
+					
+								case "7": 
+										break;
+					
+					
+								case "8": 
+										break;
+					
+					
+								case "9": 
+										break;
+					
+					
+								case "0": 
+										break;
+					
+					
+								default: 
+										return;
+										break;
+								}
+								label_ = value;
+
+						}
+
+				}
 				// Use this for initialization
 				void Start ()
 				{
