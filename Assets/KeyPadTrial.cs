319d18925740802e4be5b38e227e181137214e45
Gabriel Hare <ghare@leapmotion.com>
2014-10-07 11:40:23 -0700

    Tying button clicks to counter

--- Assets/KeyPadTrial.cs
+++ Assets/KeyPadTrial.cs
@@ -9,8 +9,20 @@ namespace P1
 		public class KeyPadTrial : MonoBehaviour
 		{
 				Stopwatch timer;
-
 				public ButtonCounter counter;
+
+				public LeapButton b0;
+				public LeapButton b1;
+				public LeapButton b2;
+				public LeapButton b3;
+				public LeapButton b4;
+				public LeapButton b5;
+				public LeapButton b6;
+				public LeapButton b7;
+				public LeapButton b8;
+				public LeapButton b9;
+				public LeapButton bStar;
+				public LeapButton bHash;
 
 				KeyPadTrial ()
 				{
@@ -34,7 +46,9 @@ namespace P1
 								label [0] == '6' ||
 								label [0] == '7' ||
 								label [0] == '8' ||
-								label [0] == '9') {
+								label [0] == '9' ||
+								label [0] == '*' ||
+								label [0] == '#') {
 								return true;
 						}
 						return false;
@@ -56,12 +70,22 @@ namespace P1
 	
 	
 	#region loop
-				LeapButton broadcaster;
 
 				// Use this for initialization
 				void Start ()
 				{
-						broadcaster.ButtonEventBroadcaster += OnButtonClick;
+						b0.ButtonEventBroadcaster += OnButtonClick;
+						b1.ButtonEventBroadcaster += OnButtonClick;
+						b2.ButtonEventBroadcaster += OnButtonClick;
+						b3.ButtonEventBroadcaster += OnButtonClick;
+						b4.ButtonEventBroadcaster += OnButtonClick;
+						b5.ButtonEventBroadcaster += OnButtonClick;
+						b6.ButtonEventBroadcaster += OnButtonClick;
+						b7.ButtonEventBroadcaster += OnButtonClick;
+						b8.ButtonEventBroadcaster += OnButtonClick;
+						b9.ButtonEventBroadcaster += OnButtonClick;
+						bStar.ButtonEventBroadcaster += OnButtonClick;
+						bHash.ButtonEventBroadcaster += OnButtonClick;
 
 						timer.Start ();
 				}
