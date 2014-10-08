2288ab5b76d0c577a0d7416669d4ea45087ef05a
David Edelhart <dave@wonderlandlabs.com>
2014-10-07 18:41:09 -0700

    adding a turnkey for configuration of the test

---
+++ Assets/Editor/TestConfigUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ButtonMonkey
+{
+		class TestConfigUnitTest
+		{
+
+			#region state
+
+				[Test]
+
+				public void TrialConfigLoaderTest ()
+				{
+						TrialConfigLoader l = new TrialConfigLoader ("Assets/testFiles/config/trial_config.json");
+						Assert.AreEqual (7, l.testDigits, "seven test digits");
+						Assert.AreEqual (9, l.trialCount, "nine trials");
+				}
+		
+		#endregion
+		
+		}
+}
