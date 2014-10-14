f9b9b08f5f69f1e24997aad061428a5d903929e4
David Edelhart <dave@wonderlandlabs.com>
2014-10-14 13:33:39 -0700

    blocking random number replication

--- Assets/Scripts/Stage1/GridMonkey.cs
+++ Assets/Scripts/Stage1/GridMonkey.cs
@@ -6,9 +6,23 @@ namespace P1
 {
 		public class GridMonkey : MonkeyTester
 		{
-				protected override List<int> GenerateKeys ()
+
+				int last_ = -1;
+
+				int Next (int range)
 				{
 						System.Random gen = new System.Random ();
+						int next = -1;
+
+						do {
+								next = gen.Next ();
+						} while (next == last_);
+						last_ = next;	
+						return next;
+				}
+
+				protected override List<int> GenerateKeys ()
+				{
 
 						int row = testConfig ["grid"] ["row"].AsInt;
 						int col = testConfig ["grid"] ["col"].AsInt;
@@ -26,7 +40,7 @@ namespace P1
 						while (keys.Count < pins) {
 								List<int> picks = new List<int> ();
 								for (int r = 0; r < row; ++r) {
-										int num = 1 + (r * col) + (gen.Next () % col);
+										int num = 1 + (r * col) + (Next (col));
 										if (num < 10) {
 												picks.Add (num);
 										} else {
@@ -36,7 +50,7 @@ namespace P1
 				
 								// Choose a random ordering of rows
 								while (picks.Count > 0 && keys.Count < pins) {
-										int next = gen.Next () % picks.Count;
+										int next = Next (picks.Count);
 										keys.Add (picks [next]);
 										picks.RemoveAt (next);
 								}
