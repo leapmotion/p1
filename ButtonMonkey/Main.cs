49423fe7f4cf7e3d61d03f5125e2219ad44ab10a
Gabriel Hare <ghare@leapmotion.com>
2014-10-16 13:41:06 -0700

    Repaired stand-alone functionality of ButtonMonkey

--- ButtonMonkey/Main.cs
+++ ButtonMonkey/Main.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 
 namespace ButtonMonkey
 {
+	public class ConsoleMonkey : MonkeyTester
+	{
+		protected override List<int> GenerateKeys ()
+		{
+			System.Random gen = new System.Random ();
+			int trialNum = testConfig ["trialNum"].AsInt;
+
+			List<int> keys = new List<int> ();
+			for (int trial = 0; trial < trialNum; ++trial) {
+				keys.Add (gen.Next () % 10);
+			}
+			return keys;
+		}
+	}
+
 	class MainClass
 	{
 		public static void Main (string[] args)
@@ -11,34 +26,67 @@ namespace ButtonMonkey
 				Console.WriteLine ("No monkeys? No research!");
 				return;
 			#else
-			ButtonTrial monkeyDo = new ButtonTrial();
+			ConsoleMonkey monkeyDo = new ConsoleMonkey ();
+			monkeyDo.ConfigureTest ("console");
+			monkeyDo.Start ();
+			Console.WriteLine ("\nMonkey, type: " + monkeyDo.GetTrialKeysString ());
 
-			int test = 1;
-			while (test <= 4) {
-				// Initial instructions
-				monkeyDo.Start ();
-				Console.WriteLine ("\nMonkey, type: " + monkeyDo.GetTrialKeysString ());
-
-				while (true) {
-					ConsoleKeyInfo info = Console.ReadKey ();
-					monkeyDo.WhenPushed (true, info.KeyChar); //No partial key strokes
-					
-					// Monkey guidance
-					if (monkeyDo.IsComplete ()) {
-						Console.WriteLine ("\nWell done, monkey!\n");
-						break;
-					} else {
-						if (monkeyDo.WasCorrect ()) {
-							Console.WriteLine ("\nGood monkey! Next, type: " + monkeyDo.GetTargetKey ());
-						} else {
-							Console.WriteLine ("\nBad monkey! You were told to type: " + monkeyDo.GetTargetKey ());
-						}
-					}
+			while (true) {
+				if (monkeyDo.StageComplete ()) {
+					Console.WriteLine ("\nWell done monkey! Close you eyes and we'll give you a treat...");
+					break; //End testing
+				}
+				if (monkeyDo.TrialComplete ()) {
+					monkeyDo.Start (); //Start next trial
+					Console.WriteLine ("\nMonkey, type: " + monkeyDo.GetTrialKeysString ());
 				}
 
-				test += 1;
+				ConsoleKeyInfo info = Console.ReadKey ();
+				int symbol = -1; //DEFAULT: Key is not a digit
+				switch (info.KeyChar) {
+				case '1': 
+					symbol = 1;
+					break;
+				case '2': 
+					symbol = 2;
+					break;
+				case '3': 
+					symbol = 3;
+					break;
+				case '4': 
+					symbol = 4;
+					break;
+				case '5': 
+					symbol = 5;
+					break;
+				case '6': 
+					symbol = 6;
+					break;
+				case '7': 
+					symbol = 7;
+					break;
+				case '8': 
+					symbol = 8;
+					break;
+				case '9': 
+					symbol = 9;
+					break;
+				case '0': 
+					symbol = 0;
+					break;
+				default:
+					break; //DEFAULT
+				}
+				monkeyDo.WhenPushed (true, symbol); //No partial key strokes
+
+				if (!monkeyDo.TrialComplete ()) {
+					if (monkeyDo.WasCorrect ()) {
+						Console.WriteLine ("\nGood monkey! Next, type: " + monkeyDo.GetTrialKeys () [monkeyDo.GetTrialStep ()]);
+					} else {
+						Console.WriteLine ("\nBad monkey! You were told to type: " + monkeyDo.GetTrialKeys () [monkeyDo.GetTrialStep ()]);
+					}
+				}
 			}
-			
 			Console.WriteLine ("Autopsy report for monkey:\n\n" + monkeyDo.ToString ());
 			#endif
 		}
